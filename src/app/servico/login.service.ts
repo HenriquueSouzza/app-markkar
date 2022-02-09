02d8a84c822d2afd1c024615febf3a90fe343626
HenriquueSouzza <henrique.liga@gmail.com>
2022-02-09 17:36:27 -0300

    login init

--- src/app/servico/login.service.ts
+++ src/app/servico/login.service.ts
@@ -1,4 +1,3 @@
-import { Login } from './login.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -11,15 +10,11 @@ export interface Login{
   providedIn: 'root'
 })
 export class LoginService {
-  private urlFrtLogin = 'http://192.168.1.93/firstLogin.php';
-  private urlLogin = '';
+  private url = 'http://192.168.1.93/firstLogin.php';
+
   constructor(private http: HttpClient) { }
 
   firstlogin(login: Login){
-    return this.http.post(this.urlFrtLogin, login);
-  }
-  //terminar isso
-  Login(login: Login){
-    return this.http.post(this.urlLogin, login);
+    return this.http.post(this.url, login);
   }
 }
