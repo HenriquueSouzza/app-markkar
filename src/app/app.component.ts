e07f390d0ea84fca8fc02f3cd645900d92fcdfe7
HenriquueSouzza <henrique.liga@gmail.com>
2022-02-17 17:24:42 -0300

    home page and splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SplashScreen} from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   constructor() {}
 }
+setTimeout(() => {
+  SplashScreen.hide();
+}, 2000);
