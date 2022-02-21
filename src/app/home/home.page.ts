4526175485b3e4e36485180eeaecc7f260ada5b0
HenriquueSouzza <henrique.liga@gmail.com>
2022-02-21 17:02:26 -0300

    home updates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,15 @@
 /* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/quotes */
+/* eslint-disable @typescript-eslint/dot-notation */
 import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
 import { Storage } from '@ionic/storage-angular';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
+import { MenuController } from '@ionic/angular';
 import { StorageService } from './../servico/storage.service';
+import { LoginService } from './../servico/login.service';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -14,10 +18,46 @@ import { StorageService } from './../servico/storage.service';
 })
 export class HomePage implements OnInit {
 
-  constructor(private menu: MenuController, private storage: Storage, private storageService: StorageService, private router: Router, public alertController: AlertController) { }
+  constructor(private service: LoginService, public loadingController: LoadingController, private menu: MenuController, private storage: Storage, private storageService: StorageService, private router: Router, public alertController: AlertController) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.menu.enable(true, 'homeMenu');
+    const valFLogin = await this.storage.get('fOpen');
+    const valFCNPJ = await this.storage.get('fCNPJ');
+    const valFSenha = await this.storage.get('fSenha');
+    const valDataBase = await this.storage.get('dataBase');
+    const valLogin = await this.storage.get('login');
+    const valSenhaLogin = await this.storage.get('senhaLogin');
+    const validateLogin = {login: valLogin, senha: valSenhaLogin, bd: valDataBase};
+    const validatefLogin = {cnpj: valFCNPJ, senha: valFSenha};
+    if(valFLogin !== false){
+      this.router.navigateByUrl('/welcome', { replaceUrl: true });
+    }
+    else if(valDataBase !== null && valLogin !== null && valSenhaLogin !== null){
+      this.service.login(validateLogin).subscribe(async response =>{
+        if(response["status"] === 'success'){
+        }
+        else if(response["status"] === 'failed'){
+          this.router.navigateByUrl('/login', { replaceUrl: true });
+        }
+        else if(response["status"] === 'errDB'){
+          this.router.navigateByUrl('/login-empresa', { replaceUrl: true });
+        }
+      });
+    }
+    else if(valFCNPJ !== null && valFSenha !== null){
+      this.service.firstlogin(validatefLogin).subscribe(async response =>{
+        if(response["dataBase"] == null){
+          this.router.navigateByUrl('/login-empresa', { replaceUrl: true });
+        }
+        else{
+          this.router.navigateByUrl('/login', { replaceUrl: true });
+        }
+      });
+    }
+    else{
+      this.router.navigateByUrl('/login-empresa', { replaceUrl: true });
+    }
   }
 
   async logOut(): Promise<void>{
@@ -47,5 +87,12 @@ export class HomePage implements OnInit {
       ]
     });
     await alert.present();
+  }
+  doRefresh(event) {
+    //func
+    setTimeout(() => {
+      //returm
+      event.target.complete();
+    }, 2000);
   }
 }
