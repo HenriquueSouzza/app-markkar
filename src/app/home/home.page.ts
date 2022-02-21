c21875bc23939ce3041f9283346e0ff026fcf78d
HenriquueSouzza <henrique.liga@gmail.com>
2022-02-21 15:45:41 -0300

    log-out home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,11 @@
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/quotes */
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { StorageService } from './../servico/storage.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +14,38 @@ import { MenuController } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
-  constructor(private menu: MenuController) { }
+  constructor(private menu: MenuController, private storage: Storage, private storageService: StorageService, private router: Router, public alertController: AlertController) { }
 
   ngOnInit() {
     this.menu.enable(true, 'homeMenu');
+  }
+
+  async logOut(): Promise<void>{
+    await this.storage.remove("login");
+    await this.storage.remove("senhaLogin");
+    this.router.navigateByUrl('/login', { replaceUrl: true });
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Desconectar',
+      message: 'Você realmente deseja se desconectar da sua conta ?',
+      buttons: [
+        {
+          text: 'NÃO',
+          role: 'cancel',
+          cssClass: 'secondary',
+          id: 'cancel-button'
+        }, {
+          text: 'SIM',
+          id: 'confirm-button',
+          handler: () => {
+            this.logOut();
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 }
