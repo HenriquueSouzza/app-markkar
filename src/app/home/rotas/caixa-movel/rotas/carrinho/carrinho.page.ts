e4396be7999eaf20de87c64a8a0a1fde5603f564
henriquueSouzza <henrique.liga@gmail.com>
2022-09-03 12:49:01 -0300

    updates carrinho

--- src/app/home/rotas/caixa-movel/rotas/carrinho/carrinho.page.ts
+++ src/app/home/rotas/caixa-movel/rotas/carrinho/carrinho.page.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/dot-notation */
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,11 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class CarrinhoPage implements OnInit {
 
   public produtos: Array<object>;
+  public totalCarrinho: string;
 
   constructor() { }
 
   ngOnInit() {
     this.produtos = [{nome: 'teste', id: 1, cod: 2123, qnt: 5, valor: 18}, {nome: 'teste2', id: 5, cod: 2323, qnt: 3, valor: 52}];
+    this.totalCar();
   }
 
   toIntQnt(qnt){
@@ -25,9 +28,9 @@ export class CarrinhoPage implements OnInit {
 
   changeQnt(qnt, exc){
     if (exc === 'add') {
-      qnt = qnt + 1;
+      qnt++;
     } else if (exc === 'sub') {
-      qnt = qnt - 1;
+      qnt--;
     }
     if(qnt < 1 || qnt === ''){
       return 1;
@@ -41,5 +44,25 @@ export class CarrinhoPage implements OnInit {
       style: 'currency',
       currency: 'BRL',
     });
+  }
+
+  totalCar(){
+    const valores = [];
+    for (const produto of this.produtos) {
+      valores.push(produto['valor']*produto['qnt']);
+      this.totalCarrinho = this.convertReal(valores.reduce((a, b) => a + b, 0));
+    }
+  }
+
+  remove(prodEvent){
+    console.log(prodEvent);
+    let i = -1;
+    for (const produto of this.produtos) {
+      i++;
+      if(produto['id'] === prodEvent['id']){
+        console.log(i);
+        this.produtos.splice(i,i);
+      }
+    }
   }
 }
