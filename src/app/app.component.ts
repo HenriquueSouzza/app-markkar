886dd34a420190001706786234eb241eb677a6ae
henriquueSouzza <henrique.liga@gmail.com>
2023-06-16 17:49:43 -0300

    login resolvido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { isPlatform } from '@ionic/angular';
 import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
 import { ControleVersaoService } from './services/controleVersao/controle-versao.service';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-root',
--- src/app/login/rotas/bem-vindo/bem-vindo.component.ts
+++ src/app/login/rotas/bem-vindo/bem-vindo.component.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/dot-notation */
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Storage } from '@ionic/storage-angular';
-import { MenuController, LoadingController, isPlatform} from '@ionic/angular';
+import { MenuController, LoadingController, isPlatform } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { Platform } from '@ionic/angular';
 import { LoginService } from '../../services/login/login.service';
@@ -14,7 +14,6 @@ import { StorageService } from 'src/app/services/storage/storage.service';
   styleUrls: ['./bem-vindo.component.scss'],
 })
 export class BemVindoComponent implements OnInit {
-
   cnpjErr: string;
   err: string;
   colorCnpj: string;
@@ -24,134 +23,132 @@ export class BemVindoComponent implements OnInit {
   private auth: any;
 
   constructor(
-    private service: LoginService,
     public loadingController: LoadingController,
+    private service: LoginService,
     private storageService: StorageService,
     private storage: Storage,
     private menu: MenuController,
     private router: Router,
     private platform: Platform
-    ) {
-      this.platform.keyboardDidShow.subscribe(ev => {
-        const { keyboardHeight } = ev;
-        if(!isPlatform('ios')){
-          if(platform.height() <= 500){
-            this.keyHeight = true;
-          }
-          else if(platform.height() <= 690){
-            this.keyHeightM = true;
-          }
+  ) {
+    this.platform.keyboardDidShow.subscribe((ev) => {
+      const { keyboardHeight } = ev;
+      if (!isPlatform('ios')) {
+        if (platform.height() <= 500) {
+          this.keyHeight = true;
+        } else if (platform.height() <= 690) {
+          this.keyHeightM = true;
         }
-      });
-      this.platform.keyboardDidHide.subscribe(ev => {
-        this.keyHeight = false;
-        this.keyHeightM = false;
-      });
-    }
-
-  // eslint-disable-next-line @typescript-eslint/member-ordering
-  @ViewChild('swiper') swiper: any;
-
-    slideNext(){
-        this.swiper.swiperRef.slideNext();
       }
+    });
+    this.platform.keyboardDidHide.subscribe((ev) => {
+      this.keyHeight = false;
+      this.keyHeightM = false;
+    });
+  }
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  @ViewChild('swiperContainer') swiperEl: any;
 
-    slidePrev(){
-      this.swiper.swiperRef.slidePrev();
-    }
+
+  slideNext() {
+    this.swiperEl.nativeElement.swiper.slideNext();
+  }
+
+  slidePrev() {
+    this.swiperEl.nativeElement.swiper.slidePrev();
+  }
 
   async ngOnInit() {
     this.auth = await this.storage.get('auth');
-    if(this.auth.hasOwnProperty('empresa')) {
+    if (this.auth.hasOwnProperty('empresa')) {
       const valCnpj = this.auth.empresa.cnpj;
       const valToken = this.auth.empresa.token;
-      if(valCnpj !== null && valToken !== null){
+      if (valCnpj !== null && valToken !== null) {
         this.router.navigateByUrl('/login/usuario', { replaceUrl: true });
       }
     }
   }
 
-  resetColor(){
+  resetColor() {
     this.colorTOKEN = null;
     this.colorCnpj = null;
   }
 
-  async enviarLogin(form: NgForm){
+  async enviarLogin(form: NgForm) {
     const loading = await this.loadingController.create({
-      message: 'autenticando...'
+      message: 'autenticando...',
     });
     await loading.present();
     const login = form.value;
-    if(login.cnpj.toString().length !== 14){
+    if (login.cnpj.toString().length !== 14) {
       await loading.dismiss();
-      this.swiper.swiperRef.slidePrev();
+      this.swiperEl.nativeElement.swiper.slidePrev();
       this.cnpjErr = 'Digite um CNPJ valido';
       this.colorCnpj = 'danger';
-    }
-    else if(login.token.length === 0){
+    } else if (login.token.length === 0) {
       await loading.dismiss();
       this.cnpjErr = null;
       this.colorCnpj = null;
       this.colorTOKEN = 'danger';
       this.err = 'Digite uma senha';
-    }
-    else{
+    } else {
       this.cnpjErr = null;
       this.colorTOKEN = null;
       this.colorCnpj = null;
-      this.service.firstlogin(login).subscribe(async (response: any) =>{
-        if(response.connection['status'] === 'failed'){
-          this.colorTOKEN = 'danger';
-          this.colorCnpj = 'danger';
-          this.err = 'CNPJ ou TOKEN invalido';
-          await loading.dismiss();
-        }
-        else if(response.connection['status'] === 'blocked'){
-          this.colorTOKEN = 'danger';
-          this.err = 'TOKEN bloqueado';
-          await loading.dismiss();
-        }
-        else if(response.connection['status'] === 'success'){
-          this.err = null;
-          this.colorTOKEN = null;
-          this.colorCnpj = null;
-          let auth = this.auth;
-          if(auth === null){
-            auth = {};
-            auth.empresa = {
+      this.service.firstlogin(login).subscribe(
+        async (response: any) => {
+          if (response.connection['status'] === 'failed') {
+            this.colorTOKEN = 'danger';
+            this.colorCnpj = 'danger';
+            this.err = 'CNPJ ou TOKEN invalido';
+            await loading.dismiss();
+          } else if (response.connection['status'] === 'blocked') {
+            this.colorTOKEN = 'danger';
+            this.err = 'TOKEN bloqueado';
+            await loading.dismiss();
+          } else if (response.connection['status'] === 'success') {
+            this.err = null;
+            this.colorTOKEN = null;
+            this.colorCnpj = null;
+            let auth = this.auth;
+            if (auth === null) {
+              auth = {};
+              auth.empresa = {
+                cnpj: login.cnpj,
+                token: login.token,
+                id: response.loginInformation['id_token'],
+              };
+            } else {
+              auth.empresa = {
+                cnpj: login.cnpj,
+                token: login.token,
+                id: response.loginInformation['id_token'],
+              };
+            }
+            await this.storageService.set('auth', auth);
+            const multiEmpresa = await this.storage.get('multiEmpresa');
+            multiEmpresa.empresas[response.loginInformation['id_token']] = {
+              empresa: response.loginInformation['empresa'],
               cnpj: login.cnpj,
               token: login.token,
-              id: response.loginInformation['id_token']
+              idToken: response.loginInformation['id_token'],
+              telefone: response.loginInformation['telefone'],
+              email: response.loginInformation['email'],
             };
-          } else {
-            auth.empresa = {
-              cnpj: login.cnpj,
-              token: login.token,
-              id: response.loginInformation['id_token']
-            };
+            await this.storage.set('multiEmpresa', multiEmpresa);
+            const appConfig = await this.storage.get('appConfig');
+            appConfig.firstOpen = false;
+            appConfig.empresaAtual = response.loginInformation['empresa'];
+            await this.storage.set('appConfig', appConfig);
+            await loading.dismiss();
+            this.router.navigateByUrl('/login/usuario', { replaceUrl: true });
           }
-          await this.storageService.set('auth', auth);
-          const multiEmpresa = await this.storage.get('multiEmpresa');
-          multiEmpresa.empresas[response.loginInformation['id_token']] = {
-            empresa: response.loginInformation['empresa'],
-            cnpj: login.cnpj,
-            token: login.token,
-            idToken: response.loginInformation['id_token'],
-            telefone: response.loginInformation['telefone'],
-            email: response.loginInformation['email']
-          };
-          await this.storage.set('multiEmpresa', multiEmpresa);
-          const appConfig = await this.storage.get('appConfig');
-          appConfig.firstOpen = false;
-          appConfig.empresaAtual = response.loginInformation['empresa'];
-          await this.storage.set('appConfig', appConfig);
+        },
+        async (error) => {
           await loading.dismiss();
-          this.router.navigateByUrl('/login/usuario', { replaceUrl: true });
+          this.err = 'falha ao conectar com o servidor';
         }
-      }, async error => {
-        await loading.dismiss();
-        this.err = 'falha ao conectar com o servidor';
-      });
+      );
     }
   }
 }
